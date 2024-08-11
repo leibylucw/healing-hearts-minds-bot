e5a31aeab47ebd3bfb746bd38128153545a82c29
Luke Leiby <lucaswleiby@gmail.com>
2024-08-11 17:40:41 -0400

    Use `discord.py`'s interaction API for commands

--- src/hhmBot/hhm.py
+++ src/hhmBot/hhm.py
@@ -3,27 +3,39 @@ import os
 import discord
 import dotenv
 import requests
-
-DAD_JOKE_HEADERS = {'Accept': 'text/plain'}
+from discord import app_commands
 
 dotenv.load_dotenv()
-token = os.getenv('DISCORD_TOKEN')
+
+HHM_GUILD = discord.Object(id=os.getenv('GUILD_ID'))
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+
+class HHMBotClient(discord.Client):
+	def __init__(self, *, intents):
+		super().__init__(intents=intents)
+		self.tree = app_commands.CommandTree(self)
+
+	async def setup_hook(self):
+		self.tree.copy_global_to(guild=HHM_GUILD)
+		await self.tree.sync(guild=HHM_GUILD)
+
+
+client = HHMBotClient(intents=intents)
+
 
 @client.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+	print(f'Logged in as {client.user} (ID: {client.user.id})')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
 
-    if message.content.lower() == '$dadjoke':
-        await message.channel.send(requests.get('https://icanhazdadjoke.com/', headers=DAD_JOKE_HEADERS).text)
+@client.tree.command(name='dadjoke', description='Get a funny dad joke!')
+async def dadJoke(interaction):
+	await interaction.response.send_message(
+		requests.get('https://icanhazdadjoke.com', headers={'Accept': 'text/plain'}).text, ephemeral=True
+	)
 
-client.run(token)
+
+client.run(os.getenv('DISCORD_TOKEN'))
