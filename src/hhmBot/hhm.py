1609cf26cbb121123d1aada226733cb82b7e166f
Luke Leiby <lucaswleiby@gmail.com>
2024-08-13 15:57:03 -0400

    Add report to HHM mods context menu feature

--- src/hhmBot/hhm.py
+++ src/hhmBot/hhm.py
@@ -44,4 +44,26 @@ async def dadJoke(interaction):
 	await interaction.response.send_message(joke, ephemeral=True)
 
 
+@client.tree.context_menu(name='Report to HHM Mods')
+async def reportToHHMMods(interaction: discord.Interaction, message: discord.Message):
+	await interaction.response.send_message(
+		f'Thanks for reporting this message by {message.author.mention} to HHM mods. Someone will reach out to you shortly. Note that this message has been reported to HHM mods, not Discord itself. If you feel the offensive nature of the message is severe, please also report the message to Discord as well as HHM mods.',
+		ephemeral=True,
+	)
+
+	logChannel = interaction.guild.get_channel(1273000106253226146)
+
+	embed = discord.Embed(title=f'{interaction.user.display_name} Reported a Message')
+	if message.content:
+		embed.description = message.content
+
+	embed.set_author(name=message.author.display_name, icon_url=message.author.display_avatar.url)
+	embed.timestamp = message.created_at
+
+	urlView = discord.ui.View()
+	urlView.add_item(discord.ui.Button(label='Go to Message', style=discord.ButtonStyle.url, url=message.jump_url))
+
+	await logChannel.send(embed=embed, view=urlView)
+
+
 client.run(os.getenv('DISCORD_TOKEN'))
